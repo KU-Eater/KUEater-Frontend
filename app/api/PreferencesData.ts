41c33f31e6013daa8b69a6a847feb6315476c1d8
ProudHokori <proundkrafpom@gmail.com>
2025-03-27 03:55:29 +0700

    Add common dishedOption for user

--- app/api/PreferencesData.ts
+++ app/api/PreferencesData.ts
@@ -2,42 +2,89 @@
 // You can switch to .js if you don't need TypeScript features.
 
 export const dietaryOptions: string[] = [
-    "Halal", "Vegetarian", "Vegan","Pescatarian", 
-    "Pollotarian", "Low-Carb","Keto", "Low-Fat", "High-Protein"
+    "Halal", "Vegetarian", "Vegan", "Pescatarian",
+    "Pollotarian", "Low-Carb", "Keto", "Low-Fat", "High-Protein"
 ];
 
 export const allergiesOptions: string[] = [
     "Lactose", "Eggs", "Shellfish", "Fishes", "Seafood",
     "Peanuts", "Gluten", "Sesame", "Nuts", "Soy", "Rice",
-    "Red Meat", "Corn", "Wheat", "Fructose", "Chocolate", 
+    "Red Meat", "Corn", "Wheat", "Fructose", "Chocolate",
     "Msg"
 ];
 
 export const cuisinesOptions: string[] = [
-    "Thai", "Italian", "Chinese", "Japanese", "Indian", 
-    "Korean","Vietnamese", "Middle Eastern", "Western", 
-    "Cantonese", "Isaan","Fusion"
+    "Thai", "Italian", "Chinese", "Japanese", "Indian",
+    "Korean", "Vietnamese", "Middle Eastern", "Western",
+    "Cantonese", "Isaan", "Fusion"
 ];
 
 export const dislikesOptions: string[] = [
-    "Fruits", "Vegetables", "Fermented Fish Sauce (Pla-ra)", 
-    "Seafoods","Stink Beans (Sa-taw)", "Tomatoes", 
-    "Shrimp Paste (Ka-pi)", "Century Eggs","Durians", 
+    "Fruits", "Vegetables", "Fermented Fish Sauce (Pla-ra)",
+    "Seafoods", "Stink Beans (Sa-taw)", "Tomatoes",
+    "Shrimp Paste (Ka-pi)", "Century Eggs", "Durians",
     "Garlics", "Ripe Papaya", "Bitter Melon (Ma-ra)",
-    "Celary","Chinese Sausages", "Onions", "Bamboo Shoots", 
-    "Thai Celary", "Lemongrasses","Cartilage", "Crispy Bone", 
+    "Celary", "Chinese Sausages", "Onions", "Bamboo Shoots",
+    "Thai Celary", "Lemongrasses", "Cartilage", "Crispy Bone",
     "Offal", "Mushrooms", "Coconut Milk", "Tofu", "Bean Sprouts",
     "Nuts", "Fatty Meat", "Chicken skin", "Eggplant", "Green Bean"
 ];
 
 export const dishesOptions: string[] = [
-    "Thai Food", "Egg", "Crispy Pork", "Fried Rice Chicken",
+    "Thai Food", "Crispy Pork", "Fried Chicken",
     "Iced Thai Tea", "Pad Thai", "Vietnamese Noodle (Pho)",
-    "Thai Coconut Curry Noodle (Khao Soi)", "Noodles", "Crab", "Spicy Salad", "Sushi",
-    "Hamburger", "Coffee", "Som Tam (Green Papaya Salad)", "Kimchi soup (Kimchi Jjigae)",
-    "Japanese foods", "Chicken", "Shrimp", "Smoothies", "Mango Sticky Rice", "Fish",
-    "Kimchi Jjigae (Kimchi Soup)", "Japanese Curry Rice", "Stir-Fried Thai Basil (Pad Kra Pao)",
-    "Thai Green Curry Chicken", "Stir-Fried Noodle (Pad See Ew)", "Spaghetti",
-    "Thai Chicken Rice", "Curry Rice", "Pork"
+    "Noodles", "Crab", "Spicy Salad", "Sushi", "Dim Sum",
+    "Thai Coconut Curry Noodle (Khao Soi)", "Hamburger", 
+    "Coffee", "Som Tam (Green Papaya Salad)", "Fish Steak",
+    "Japanese foods", "Chicken", "Shrimp", "Smoothies",
+    "Teriyaki Chicken Rice"," Thai Style Suki","Boat Noodles",
+    "Mango Sticky Rice", "Japanese Curry Rice","Chicken Nuggets",
+    "Har gow (shrimp dumplings)","Yen Ta Fo (Pink Noodles)",
+    "Thai Curry with Rice (Kao Kang)", "Tom Yum Noodles","Beef Stew",
+    "Stir-Fried Thai Basil (Pad Kra Pao)","Biryani", "Seafood",
+    "Thai Green Curry Chicken", "Stir-Fried Noodle (Pad See Ew)", 
+    "Spaghetti", "Thai Chicken Rice",  "Pork", "Fried Rice",
+    "Stir-Fried Dish", "Grilled Meat", "Soup Dish", "Sandwich",
+    "Thai Curry", "Vegetarian Meal", "Clean Food", "Boiled Rice",
+    "Omelette", "Crispy Fried Dish", "Instant Noodles",
+    "Thai Chicken Rice", "Milk Tea","Yakisoba","Curry Udon",
+    "Green Curry Chicken", "Coconut milkshake", "Lime soda",
+    "Sticky Rice with Grilled Pork","Coke","Pepsi","Fruit",
+    "Garlic Pork with Rice", "Spicy mango salad",
+    "Stir-Fried Basil with Crispy Pork", "Meat Dish",
+    "Spicy Glass Noodle Salad (Yum Woon Sen)",
+    "Grilled Pork Skewers (Moo Ping)","Pork Leg Stew (Kha Moo)",
+    "Bento Box", "Strawberry milkshake", "Macaroni",
+    "Chicken with Cashew Nuts", "Rice with holy basil pork",
+    "Deep-Fried Pork Belly with Fish Sauce", "Pizza",
+    "Pork with Ginger Stir-Fry", "Panaeng Curry",
+    "Japanese Pork Cutlet (Tonkatsu) Rice", "Omelette",
+    "Steamed Fish with Soy Sauce", "Vegan Protein Dish",
+    "Tom Yum Goong (Spicy Shrimp Soup)", "Cheese Sausage",
+    "Tom Kha Gai (Chicken in Coconut Soup)", "French Fries",
+    "Boiled Rice with Pork","Stir-Fried Morning Glory",
+    "Spicy Chicken Salad Rice", "Boneless Chicken Bites",
+    "Red BBQ Pork with Rice", "Stir-Fried Basil with Chicken",
+    "Fried Rice with Crab", "Cheese", "Clear Soup Kuay Jap",
+    "Rice with cheese chicken katsu", "Pork congee",
+    "Rice Omllete with Minced Pork", "American Fried Rice",
+    "Stir-Fried Basil with Seafood", "Ice Cream", 
+    "Fried Bread with Minced Pork", "Iced Americano",
+    "Cocoa", "Stewed Egg with Rice","Spicy Salmon Salad",
+    "Healthy smoothie", "Steamed bun with minced pork",
+    "Stir-Fried Noodles in Gravy (Rad Na)", "Salmon",
+    "Shrimp Paste Fried Rice", "Stir-Fried Red Curry Pork (Pad Ped)",
+    "Spaghetti Drunken Noodles (Pad Kee Mao)", "Iced black tea",
+    "Spicy Minced Pork Salad (Larb Moo)", "Bananas",
+    "Rice with vegetarian curry", "River Prawns", "Salad",
+    "Century Egg", "Instant Noodle Salad (Yum Mama)",
+    "Stir-Fried Red Curry Pork", "Fried Egg with minced Shrimp",
+    "Clear Soup with Tofu and Minced Pork", "Sea Bass",
+    "Ramen with Pork Chashu", "Stir-Fried Wild Boar",
+    "Cheese Balls", "Watermelon smoothie",
+    "Stir-Fried Basil with Pork", "Grilled Pork with Rice",
+    "Vegetarian Stir-Fry", "Steamed Chicken Breast",
+    "Stir-Fry Crispy pork chili salt ", "Pink Milk"
+
 ];
 
